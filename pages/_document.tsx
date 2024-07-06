52c573764ab3fa29de10fcf1e14f24b00fd19096
focusotter <mtliendo@focusotter.com>
2024-07-06 13:58:08 -0500

    set og stuff

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang="en">
+			<Head>
+				<title>WCC Photo Wall</title>
+				<meta property="og:title" content="Community Events" />
+
+				<meta
+					property="og:image"
+					content="https://res.cloudinary.com/dblm6lgj0/image/upload/c_limit,w_640/f_auto/q_auto/E712CDF8-0E0C-40B6-B173-B6C3C8ABBD6F_1_105_c_esjxei?_a=BAVFB+DW0"
+				/>
+			</Head>
+
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
 }
